9aa7548c7120fb1821c0eac9bb99417725660836
QinZhen001 <qz519189636@qq.com>
2023-07-03 21:43:37 +0800

    feat: init

--- week4/test3/main.go
+++ week4/test3/main.go
@@ -12,9 +12,12 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-func main() {
+var db *gorm.DB
+
+func init() {
+	var err error
 	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
 		logger.Config{
 			SlowThreshold:             time.Second, // 慢 SQL 阈值 > 1s
 			LogLevel:                  logger.Info, // 日志级别
@@ -23,16 +26,21 @@ func main() {
 		},
 	)
 	dsn := "root:123456789@tcp(127.0.0.1:3306)/orm_test?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: newLogger,
 	})
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("连接成功...")
-	err = db.AutoMigrate(&model.Product{})
-	if err != nil {
-		panic(err)
-	}
 
+}
+
+func main() {
+	var p model.Product
+	db.First(&p, 5000)
+	fmt.Println(p.Price)
+	fmt.Println(p.Code)
+	// 软删除
+	// db.Delete(&p, 500)
 }
