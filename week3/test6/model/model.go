5eadc1ac3dda2cabfed7a9c898892ce10e73e9a1
QinZhen001 <qz519189636@qq.com>
2023-07-29 17:44:23 +0800

    feat: del

--- week3/test6/model/model.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Accountlogin struct {
-	AccountName string `json:"accountName" binding:"required,min=8,max=32"`
-	Password    string `json:"password" binding:"required,min=4,max=32"`
-}
